b34b01ef41c98d27567718e8796ac512153d339a
lazaro-pontes <laziruspoints@gmail.com>
2019-07-04 13:35:51 -0300

    Add files via upload

---
+++ frontendinsta/src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+
+import Feed from './pages/feed'
+import New from './pages/new'
+
+function Routes() {
+    return(
+        <Switch>
+            <Route path='/' exact component={Feed}></Route>
+            <Route path='/new' component={New}></Route>
+        </Switch>
+    )
+}
+export default Routes;
